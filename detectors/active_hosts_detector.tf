db6700605b2b3f34c92b86d0d4fb68f932b39cdd
Geoff Higginbottom <github@geoffh.co.uk>
2020-07-07 12:26:15 +0100

    Clean up detectors

--- detectors/active_hosts_detector.tf
+++ detectors/active_hosts_detector.tf
@@ -1,43 +1,17 @@
 resource "signalfx_detector" "active_host__detector" {
-    name = "Active Host Detector"
-    description = "Alert when the number of active hosts goes above a static threshold - deployed via Terraform"
-    max_delay = 1
-    program_text = <<-EOF
-        A = data('memory.utilization', extrapolation='last_value', maxExtrapolations=5).count().publish(label='A', enable=False)
-        detect(when(A >0)).publish('Active Hosts Detector')
-    EOF
-    rule {
-        description = "Active Hosts"
-        severity = "Minor"
-        detect_label = "Active Hosts Detector"
-        notifications = ["Email,ghigginbottom@splunk.com", "VictorOps,ERI0R2GAIAA,GH_PRI"]
-        parameterized_subject = "{{ruleSeverity}} Alert: {{{ruleName}}} ({{{detectorName}}})"
-        parameterized_body = <<EOF
-{{#if anomalous}}
-	Rule "{{{ruleName}}}" in detector "{{{detectorName}}}" triggered at {{timestamp}}.
-{{else}}
-	Rule "{{{ruleName}}}" in detector "{{{detectorName}}}" cleared at {{timestamp}}.
-{{/if}}
-
-{{#if anomalous}}
-Triggering condition: {{{readableRule}}}
-{{/if}}
-
-Minimum value of signal in the last {{event_annotations.current_window}}: {{inputs.recent_min.value}}
-{{#if anomalous}}Trigger threshold: {{inputs.f_top.value}}
-{{else}}Clear threshold: {{inputs.c_top.value}}
-{{/if}}
-
-{{#notEmpty dimensions}}
-Signal details:
-{{{dimensions}}}
-{{/notEmpty}}
-
-{{#if anomalous}}
-{{#if runbookUrl}}Runbook: {{{runbookUrl}}}{{/if}}
-{{#if tip}}Tip: {{{tip}}}{{/if}}
-{{/if}}
-EOF
-    }
-
+  name         = "Active Host Detector"
+  description  = "Alert when the number of active hosts goes above a static threshold - deployed via Terraform"
+  max_delay    = 1
+  program_text = <<-EOF
+    A = data('memory.utilization', extrapolation='last_value', maxExtrapolations=5).count().publish(label='A', enable=False)
+    detect(when(A >0)).publish('Active Hosts')
+  EOF
+  rule {
+    description           = "Active Hosts"
+    severity              = "Minor"
+    detect_label          = "Active Hosts"
+    notifications         = ["Email,ghigginbottom@splunk.com", "VictorOps,ERI0R2GAIAA,GH_PRI"]
+    parameterized_subject = "{{ruleSeverity}} Alert: {{{ruleName}}} ({{{detectorName}}})"
+    parameterized_body    = var.message_body
+  }
 }
--- variables.tf
+++ variables.tf
@@ -105,3 +105,38 @@ variable "smart_agent_version" {
 variable "traceEndpointUrl" {
   default = []
 }
+
+variable "message_body" {
+  type = string
+
+  default = <<-EOF
+    {{#if anomalous}}
+	    Rule "{{{ruleName}}}" in detector "{{{detectorName}}}" triggered at {{timestamp}}.
+    {{else}}
+	    Rule "{{{ruleName}}}" in detector "{{{detectorName}}}" cleared at {{timestamp}}.
+    {{/if}}
+
+    {{#if anomalous}}
+      Triggering condition: {{{readableRule}}}
+    {{/if}}
+
+    {{#if anomalous}}
+      Signal value: {{inputs.A.value}}
+    {{else}}
+      Current signal value: {{inputs.A.value}}
+    {{/if}}
+
+    {{#notEmpty dimensions}}
+      Signal details: {{{dimensions}}}
+    {{/notEmpty}}
+
+    {{#if anomalous}}
+      {{#if runbookUrl}}
+        Runbook: {{{runbookUrl}}}
+      {{/if}}
+      {{#if tip}}
+        Tip: {{{tip}}}
+      {{/if}}
+    {{/if}}
+  EOF
+}
