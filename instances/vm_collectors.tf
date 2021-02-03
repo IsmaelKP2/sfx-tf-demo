bcbfd2a443f5acd61013ee98fcd36c153c80f228
Geoff Higginbottom <github@geoffh.co.uk>
2021-02-03 17:30:01 +0000

    Update vm_collectors.tf

--- instances/vm_collectors.tf
+++ instances/vm_collectors.tf
@@ -47,24 +47,26 @@ resource "aws_instance" "collector" {
 
   provisioner "remote-exec" {
     inline = [
-      # Set Hostname
+    # Set Hostname
       "sudo sed -i 's/127.0.0.1.*/127.0.0.1 ${self.tags.Name}.local ${self.tags.Name} localhost/' /etc/hosts",
       "sudo hostnamectl set-hostname ${self.tags.Name}",
       "sudo apt-get update",
       "sudo apt-get upgrade -y",
 
-      # Install SignalFx
+    # Install SignalFx
       "TOKEN=${var.auth_token}",
       "REALM=${var.realm}",
       "HOSTNAME=${self.tags.Name}",
       "CLUSTERNAME=${var.cluster_name}",
       "AGENTVERSION=${var.smart_agent_version}",
+      "ENVIRONMENT=${var.environment}",
       
       "sudo chmod +x /tmp/install_sfx_agent.sh",
       "sudo /tmp/install_sfx_agent.sh $TOKEN $REALM $CLUSTERNAME $AGENTVERSION",
 
+      "sudo sed -i -e 's+intervalSeconds.*+intervalSeconds: 1+g' /etc/signalfx/agent.yaml",
+      
       "sudo chmod +x /tmp/update_sfx_environment.sh",
-      "ENVIRONMENT=${var.environment}",
       "sudo /tmp/update_sfx_environment.sh $ENVIRONMENT",
 
       "sudo mkdir /etc/signalfx/monitors",
@@ -73,7 +75,7 @@ resource "aws_instance" "collector" {
       "sudo mv /tmp/free_disk.yaml /etc/signalfx/monitors/free_disk.yaml",
       "sudo chown root:root /etc/signalfx/monitors/free_disk.yaml",
 
-      ## Install Docker
+    ## Install Docker
       "sudo apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common -y",
       "sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -",
       "sudo add-apt-repository \"deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable\"",
@@ -81,28 +83,28 @@ resource "aws_instance" "collector" {
       "sudo apt-get install docker-ce docker-ce-cli containerd.io -y",
       "sudo systemctl enable docker",
 
-      ## Add signfx-agent to Docker Group
+    ## Add signfx-agent to Docker Group
       "sudo usermod -a -G docker signalfx-agent",
       "sudo service signalfx-agent restart",
 
-      ## Set Vars for Collector
+    ## Set Vars for Collector
       "BALLAST=${var.ballast}",
       "OTELCOL_VERSION=${var.otelcol_version}",
 
-      ## Move collector.yaml to /home/ubuntu and update permissions
+    ## Move collector.yaml to /home/ubuntu and update permissions
       "sudo cp /tmp/collector.yaml /home/ubuntu/collector.yaml",
       "sudo chown -R ubuntu:ubuntu /home/ubuntu/collector.yaml",
 
-      ## Generate collector startup script so users can easily restart it (it gets created in /home/ubuntu)
+    ## Generate collector startup script so users can easily restart it (it gets created in /home/ubuntu)
       "sudo chmod +x /tmp/generate_otc_startup.sh",
       "sudo /tmp/generate_otc_startup.sh $TOKEN $BALLAST $REALM $OTELCOL_VERSION $ENVIRONMENT",
 
-      ## Run collector
+    ## Run collector
       "sudo chmod +x /home/ubuntu/otc_startup.sh",
       "sudo chown ubuntu:ubuntu /home/ubuntu/otc_startup.sh",
       "/home/ubuntu/otc_startup.sh",
 
-      ## Configure motd
+    ## Configure motd
       # "sudo apt install curl -y",
       "sudo curl -s https://raw.githubusercontent.com/signalfx/observability-workshop/master/cloud-init/motd -o /etc/motd",
       "sudo chmod -x /etc/update-motd.d/*",
